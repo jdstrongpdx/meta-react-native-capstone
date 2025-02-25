cdb36e8788c9287ccdfe650a3893af6829895219
jdstrongpdx <jdstrongpdx@gmail.com>
2025-02-25 08:50:08 -0800

    Initial commit with project files from the Working with Data course

---
+++ utils.js
@@ -0,0 +1,78 @@
+import { useRef, useEffect } from 'react';
+
+export const SECTION_LIST_MOCK_DATA = [
+    {
+      title: 'Appetizers',
+      data: [
+        {
+          id: '1',
+          title: 'Pasta',
+          price: '10',
+        },
+        {
+          id: '3',
+          title: 'Pizza',
+          price: '8',
+        },
+      ],
+    },
+    {
+      title: 'Salads',
+      data: [
+        {
+          id: '2',
+          title: 'Caesar',
+          price: '2',
+        },
+        {
+          id: '4',
+          title: 'Greek',
+          price: '3',
+        },
+      ],
+    },
+  ];
+
+/**
+ * 3. Implement this function to transform the raw data
+ * retrieved by the getMenuItems() function inside the database.js file
+ * into the data structure a SectionList component expects as its "sections" prop.
+ * @see https://reactnative.dev/docs/sectionlist as a reference
+ */
+export function getSectionListData(data) {
+  // Reduce the data to create an object with categories as keys
+  const sections = data.reduce((acc, item) => {
+    const { category, id, title, price } = item;
+
+    // If the category doesn't already exist as a key, initialize it
+    if (!acc[category]) {
+      acc[category] = {
+        title: category,
+        data: [],
+      };
+    }
+
+    // Add the item to the corresponding category
+    acc[category].data.push({
+      id: id.toString(),
+      title,
+      price,
+    });
+
+    return acc;
+  }, {});
+
+  return Object.values(sections);
+}
+
+export function useUpdateEffect(effect, dependencies = []) {
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      return effect();
+    }
+  }, dependencies);
+}
